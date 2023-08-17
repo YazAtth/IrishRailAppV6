8c0293c694f2de2c1b003475e68a8c7935962dba
YazAtth <yasith.athan@gmail.com>
2023-08-17 13:36:14 +0100

    FEAT: Mobile navbar

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,19 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+
+
+window.navbarToggle = function() {
+
+    // alert("toggled");
+
+    const navbar = document.getElementById("navbar")
+
+    if (navbar.className === "navbar dropdown-hidden") {
+        navbar.className = "navbar dropdown-active"
+    } else {
+        navbar.className = "navbar dropdown-hidden"
+    }
+
+}
