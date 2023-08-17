ef0c4525a7b3ff73d98f0c3c761d5c96cf4813bf
YazAtth <yasith.athan@gmail.com>
2023-08-17 17:54:31 +0100

    STYLE: train-route page

--- app/controllers/train_route_controller.rb
+++ app/controllers/train_route_controller.rb
@@ -11,6 +11,8 @@ class TrainRouteController < ApplicationController
     current_date = Date.today.strftime("%e %b %Y").strip
     train_movement_endpoint_uri = "http://api.irishrail.ie/realtime/realtime.asmx/getTrainMovementsXML?TrainId=#{@train_code}&TrainDate=#{current_date}"
 
+    p train_movement_endpoint_uri
+
     departures_controller = DeparturesController.new
     @train_route = departures_controller.make_request(train_movement_endpoint_uri)["body"]["ArrayOfObjTrainMovements"]["objTrainMovements"]
 
