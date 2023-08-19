dc56cdec06d23602026acfa2b67e59be6ae9a258
YazAtth <yasith.athan@gmail.com>
2023-08-19 21:27:04 +0100

    FEAT: Added saved journeys to departure-filter page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :saved_journeys
+  has_many :saved_journeys, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
