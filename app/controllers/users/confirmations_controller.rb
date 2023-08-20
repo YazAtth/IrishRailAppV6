29d89a1324bb01826adf3b6a8925a5d253693bb3
YazAtth <yasith.athan@gmail.com>
2023-08-20 13:27:50 +0100

    FIX: Changed confirmation email redirect

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -25,7 +25,7 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
 
   # The path used after confirmation.
   def after_confirmation_path_for(resource_name, resource)
-    new_user_session_path
+    user_session_path
     # super(resource_name, resource)
   end
 end
