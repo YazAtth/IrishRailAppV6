60243327d088de45ac2b4d759e4dad7cf5d2eb44
YazAtth <yasith.athan@gmail.com>
2023-08-20 13:23:19 +0100

    FIX: Changed confirmation email redirect

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: "users/registrations",
     sessions: "users/sessions",
-    omniauth_callbacks: "users/omniauth_callbacks"
+    omniauth_callbacks: "users/omniauth_callbacks",
+    confirmations: "users/confirmations"
   }
 
   root "departures_filter#index"
