3a102e848e69bbb7dbd006b594b786737aabd9a0
YazAtth <yasith.athan@gmail.com>
2023-08-20 13:17:31 +0100

    FIX: Changed confirmation email redirect

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
 
   # Devise section
   config.action_mailer.default_url_options = {
-    host: 'irishrailapp.com',
+    host: 'www.irishrailapp.com',
   }
   config.action_mailer.delivery_method = :smtp
 
